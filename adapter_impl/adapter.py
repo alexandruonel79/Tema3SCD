81840e9b29b3e7279dd4f2d665c7880fd4cb83ec
alexandruonel79 <alexandruonel79@gmail.com>
2024-12-29 01:20:26 +0200

    Update. Trebuie adaugate retelele.

--- adapter_impl/adapter.py
+++ adapter_impl/adapter.py
@@ -8,29 +8,26 @@ from db_func import save_to_db
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 
-BROKER_HOST = "host.docker.internal"
-BROKER_PORT = 1883
+# problema pe windows doar, pe linux merge...
+# BROKER_HOST = "host.docker.internal"
+logger = logging.getLogger("Info_Logger")
 
 def setup_logging():
-    load_dotenv()
     if os.getenv('DEBUG_DATA_FLOW') == 'true':
-        # print("Logging is enabled!")
-        # Configure the logger to log INFO level and above
         logging.basicConfig(
-            level=logging.INFO,  # Log only INFO and above
-            format='%(asctime)s - %(message)s',  # Log message format with timestamp and message
+            level=logging.INFO,
+            format='%(asctime)s - %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S',
-            stream=sys.stdout  # Define timestamp format without milliseconds
+            stream=sys.stdout
         )
         logger.info("Logging is enabled!")
     else:
         logger.disabled = True
-        # print("Logging is disabled!")
 
 def on_connect(client: mqtt.Client, userdata, flags, rc: int) -> None:
-    if rc == mqtt.CONNACK_ACCEPTED:  # Use the built-in constant for connection success
+    if rc == mqtt.CONNACK_ACCEPTED:
         print("Conectat la broker!")
-        client.subscribe('#')  # Subscribe to all topics
+        client.subscribe('#')
     else:
         print(f"Eroare la conectare: {rc}")
 
@@ -40,8 +37,6 @@ def is_valid_topic(topic):
 
 def get_timestamp(payload):
     if payload.get('timestamp'):
-        # return int 
-        # return datetime.fromisoformat(payload['timestamp']).isoformat()
         logger.info("Data timestamp is: %s", payload['timestamp'])
         return payload['timestamp']
     else:
@@ -61,51 +56,55 @@ def clean_payload(old_payload):
 
 def on_message(client: mqtt.Client, userdata, msg: mqtt.MQTTMessage) -> None:
     logger.info("Received a message by topic [%s]", msg.topic)
-    # print(f"\nMesaj primit pe topicul '{msg.topic}': {msg.payload.decode()}")
-    # Check the topic, it should contain only one '/'
+
     if not is_valid_topic(msg.topic):
         print("Invalid topic!")
         return
 
-    # check if its a json
     try:
         payload = json.loads(msg.payload.decode())
-        # print("Payload: ", payload)
         json_payload = []
         location = msg.topic.split('/')[0]
         device = msg.topic.split('/')[1]
         timestamp = get_timestamp(payload)
         payload = clean_payload(payload)
 
-        for key, value in payload.items():
-            logger.info("%s.%s.%s %s", location, device, key, value)
+        for metric, data in payload.items():
+            logger.info("%s.%s.%s %s", location, device, metric, data)
             data = {
-                "measurement": f"{location}_{device}_{key}",
+                "measurement": f"{location}_{device}_{metric}",
                 "tags": {
                     "location": location,
                     "device": device,
-                    "metric": key
+                    "metric": metric
                     },
                 "time": timestamp,
                 "fields": {
-                    "data": value
+                    "data": data
                 }
             }
             json_payload.append(data)
-        # print("Payload to be saved: ", json_payload)
         save_to_db(json_payload)
 
     except json.JSONDecodeError:
         print("Mesajul nu este JSON!")
         return
 
-logger = logging.getLogger("Info_Logger")
-setup_logging()
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+def main():
+    load_dotenv()
+    BROKER_HOST = 'mqtt_broker'
+    BROKER_PORT = 1883
 
-client.connect(BROKER_HOST, BROKER_PORT)
+    setup_logging()
 
-client.loop_forever()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(BROKER_HOST, BROKER_PORT)
+
+    client.loop_forever()
+
+if __name__ == "__main__":
+    main()
--- adapter_impl/db_func.py
+++ adapter_impl/db_func.py
@@ -1,15 +1,17 @@
 # https://www.youtube.com/watch?v=49hKs_H5Xf0&t=25s&ab_channel=DevOpsJourney
+import os
 from influxdb import InfluxDBClient
 
+DATABASE = 'tema3'
 #Setup database
 db_client = InfluxDBClient('influxdb', 8086, None, None)
 
 # Check if the database exists and create it if necessary
 databases = db_client.get_list_database()
-if not any(db['name'] == 'mydb' for db in databases):
-    db_client.create_database('mydb')
+if not any(db['name'] == DATABASE for db in databases):
+    db_client.create_database(DATABASE)
 
-db_client.switch_database('mydb')
+db_client.switch_database(DATABASE)
 
 def save_to_db(payload):
     # print("Payload will be saved to db: ", payload)
